f438217495989c8d236e1d58f7d8c51c8206ccfd
azanahmed07 <aazan7391@gmail.com>
2024-11-30 03:15:56 -0800

    Initial commit

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Image from 'next/image'
-import mypic from "@/public/AZAN.jpg"
+
 const Hero = () => {
   return (
-    <div id="hero" className='min-h-screen bg-no-repeat bg-[url(/Azan.jpg)] bg-left lg:bg-[15%] bg-cover'
+    <div id="hero" className='min-h-screen bg-no-repeat bg-[url(@/public/Azan.jpg)] bg-left lg:bg-[15%] bg-cover'
     style={{backgroundSize: "35%"}} 
     >
       <Navbar />
       <div className='container grid lg:grid-cols-2 h-[calc(100vh-60px)]'>
         <div className='hidden lg:block'>
-          <Image src={mypic} alt='mypic'/>
+          
         </div>
         <div className='text-[80px] sm:text-[100px] font-bold leading-tight flex justify-center items-center'>
           <div>
